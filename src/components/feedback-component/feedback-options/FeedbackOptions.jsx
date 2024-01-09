6c96c85d38e8bda75b0cd60dd56baafb1fe8f801
Kyrylo <kyrylo.mm@gmail.com>
2024-01-09 12:58:31 +0100

    Update FeedbackOptions.jsx

--- src/components/feedback-component/feedback-options/FeedbackOptions.jsx
+++ src/components/feedback-component/feedback-options/FeedbackOptions.jsx
@@ -1,34 +1,19 @@
 // Імпорт
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { StyledButtonGroup, StyledButton } from './feedbackoptionsstyles';
 // Функція фідбеку для кнопок
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const [feedbackType, setFeedbackType] = useState(null);
-
-  const handleClick = option => {
-    setFeedbackType(option);
-  };
-
-  useEffect(() => {
-    if (feedbackType !== null) {
-      onLeaveFeedback(feedbackType);
-      setFeedbackType(null);
-    }
-  }, [feedbackType, onLeaveFeedback]);
-
-  return (
-    <StyledButtonGroup>
-      {options.map(option => (
-        <StyledButton
-          key={option}
-          onClick={() => handleClick(option)}
-          feedbackType={option}
-        >
-          {option}
-        </StyledButton>
-      ))}
-    </StyledButtonGroup>
-  );
+  <StyledButtonGroup>
+    {options.map(option => (
+      <StyledButton
+        key={option}
+        onClick={() => onLeaveFeedback(option)}
+        feedbackType={option}
+      >
+        {option}
+      </StyledButton>
+    ))}
+  </StyledButtonGroup>;
 };
 // Експорт
 export default FeedbackOptions;
